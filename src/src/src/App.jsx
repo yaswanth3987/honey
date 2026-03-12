47924a6585499f0576f338e23fe97f72f4ad20bd
yaswanth3987 <mrmyaswanth@gmail.com>
2026-03-12 15:34:22 +0530

    Create App component for luxury fashion website

---
+++ src/src/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Honey - Premium Luxury Fashion Website</h1>
+      <p>Your website is successfully deployed 🚀</p>
+    </div>
+  );
+}
+
+export default App;
