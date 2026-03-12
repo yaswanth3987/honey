125c1c196eee3c9000e7da6ae58852a3592e1f88
yaswanth3987 <mrmyaswanth@gmail.com>
2026-03-12 15:23:38 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>Hello Honey Website 🚀</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
